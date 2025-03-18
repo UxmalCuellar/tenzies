3d42b947b57c729dfc13cd239a8eaf8ad610ce18
Uxmal <uxmal.c@gmail.com>
2025-03-18 22:36:07 +1100

    create 10 random numbers and assign them to the Die components value

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import Die from './Die'
 
 export default function Main() {
-  const value = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+  const [dice, setDice] = useState(generateAllNewDice())
+  const diceElements = dice.map(number => <Die value={number}/> )
 
   function generateAllNewDice() {
-
-    const randomDice = Array(6).fill(Math.floor(Math.random() * (6 - 1 + 1) + 1))
-    console.log(randomDice)
+    return Array.from({length: 10}, () => Math.ceil(Math.random() * 6))
   }
 
   return (
     <>
-      <main className="container">
+      <main>
         <h1>Tenzies</h1>
         <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
 
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
-        <Die value={value}/>
+        <div className="die-container">
+
+          {diceElements}
+        </div>
       </main>
 
     </>
